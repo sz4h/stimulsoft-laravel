c255d0847e99f690f821421ef7bd49301dd01fc5
Ahmed Safaa <ahmed@sz4h.com>
2023-04-17 13:48:40 +0300

    Running example report template with data

--- routes/routes.php
+++ routes/routes.php
@@ -4,6 +4,6 @@
 use Illuminate\Support\Facades\Route;
 use Space\Stimulsoft\Facades\Stimulsoft;
 $report = Stimulsoft::make()
-    ->setTemplate(base_path('reports/SimpleList.mrt'))
+    ->setTemplate('reports/SimpleList.mrt')
     ->view();
 Route::get('test',fn() => $report);
